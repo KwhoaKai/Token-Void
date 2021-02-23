fcc978256a849f9d13dc271e60673501eeb651d7
eyk8 <kuo.yikai@gmail.com>
2021-02-23 00:47:44 -0500

    Add wiggly title text, bg eths, smooth intro fadeout

--- src/components/Greeting.vue
+++ src/components/Greeting.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="greetDiv">
-    <h2>{{ title }}</h2>
+    <div id="titleDiv"></div>
+    <!-- <h2>{{ title }}</h2> -->
     <h3>{{ message }}</h3>
   </div>
 </template>
@@ -13,6 +14,76 @@ module.exports = {
     message: String,
     title: String,
   },
+  watch: {
+    title: function (newTitle, oldTitle) {
+      const titleDiv = document.getElementById("titleDiv");
+      while (titleDiv.firstChild) {
+        titleDiv.removeChild(titleDiv.lastChild);
+      }
+      var text = new Blotter.Text(`${newTitle}`, {
+        family: "'EB Garamond', serif",
+        size: 80,
+        fill: "#FFFFFF",
+      });
+
+      var material = new Blotter.LiquidDistortMaterial();
+
+      // Play with the value for uSpeed. Lower values slow
+      // down animation, while higher values speed it up. At
+      // a speed of 0.0, animation is stopped entirely.
+      material.uniforms.uSpeed.value = 0.25;
+      material.uniforms.uVolatility.value = 0.015;
+
+      // Try uncommenting the following line to play with
+      // the "volatility" of the effect. Higher values here will
+      // produce more dramatic changes in the appearance of your
+      // text as it animates, but you will likely want to keep
+      // the value below 1.0.
+      //material.uniforms.uVolatility.value = 0.30;
+
+      var blotter = new Blotter(material, {
+        texts: text,
+      });
+
+      var elem = document.getElementById("titleDiv");
+      console.log(elem);
+      var scope = blotter.forText(text);
+
+      scope.appendTo(elem);
+    },
+  },
+  mounted() {
+    var text = new Blotter.Text("Welcome home, Crypto Cowboy", {
+      family: "'EB Garamond', serif",
+      size: 80,
+      fill: "#FFFFFF",
+    });
+
+    var material = new Blotter.LiquidDistortMaterial();
+
+    // Play with the value for uSpeed. Lower values slow
+    // down animation, while higher values speed it up. At
+    // a speed of 0.0, animation is stopped entirely.
+    material.uniforms.uSpeed.value = 0.25;
+    material.uniforms.uVolatility.value = 0.015;
+
+    // Try uncommenting the following line to play with
+    // the "volatility" of the effect. Higher values here will
+    // produce more dramatic changes in the appearance of your
+    // text as it animates, but you will likely want to keep
+    // the value below 1.0.
+    //material.uniforms.uVolatility.value = 0.30;
+
+    var blotter = new Blotter(material, {
+      texts: text,
+    });
+
+    var elem = document.getElementById("titleDiv");
+    var scope = blotter.forText(text);
+
+    scope.appendTo(elem);
+  },
+
   methods: {
     loadingWallet() {
       this.message = "Loading MetaMask wallet info";
@@ -22,7 +93,17 @@ module.exports = {
 </script>
 <style scoped>
 #greetDiv {
-  /* font-family: "Roboto"; */
+  font-family: "Nanum Myeongjo", serif;
+  width: 100%;
+  /* font-family: "Shippori Mincho", serif; */
+}
+
+#titleDiv {
+  width: 100%;
+}
+
+canvas {
+  width: 100%;
 }
 
 h2 {
@@ -32,5 +113,6 @@ h2 {
 
 h3 {
   color: rgb(255, 255, 255);
+  font-family: "Arial";
 }
 </style>
--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -41,6 +41,7 @@ export default {
   destroyed() {
     window.removeEventListener("resize", this.handleResize);
   },
+
   methods: {
     initThree() {
       const canvas = document.getElementById("canvas");
@@ -66,15 +67,44 @@ export default {
       });
       this.renderer.setSize(width, height);
 
+      // const geomSphere = new THREE.SphereGeometry(1, 12, 7);
+      // const matSphere = new THREE.MeshBasicMaterial({
+      //   color: 0xffffff,
+      //   wireframe: true,
+      //   opacity: 1,
+      // });
+      // const sphere = new THREE.Mesh(geomSphere, matSphere);
+      // sphere.position.y = 1;
+      // sphere.position.z = 1;
+      // this.scene.add(sphere);
+
       const loader = new GLTFLoader();
       const ethurl = "./ethlogo.gltf";
       const coinurl = "./coin1.gltf";
 
+      this.introBgObjects = [];
+      console.log(this.introBgObjects);
+
       loader.load(ethurl, (gltf) => {
-        const root = gltf.scene;
+        let logo = this.cloneGltf(gltf);
+        const root = logo.scene;
         this.introEthLogo = root;
         this.scene.add(this.introEthLogo);
         console.log("added eth to scene");
+
+        // // Add eth logos to bg
+        for (let i = 0; i < 20; i++) {
+          // Apply new material to texture
+          let model = this.cloneGltf(gltf).scene;
+          let newMaterial = new THREE.MeshStandardMaterial({
+            color: "rgb(31, 33, 33)",
+            roughness: 0,
+          });
+          model.traverse((o) => {
+            if (o.isMesh) o.material = newMaterial;
+          });
+          this.addIntroBgObject(model);
+        }
 
         // from root and below
         const box = new THREE.Box3().setFromObject(root);
@@ -85,6 +115,7 @@ export default {
 
         this.introEthLogo.scale.multiplyScalar((1 / boxSize) * 2);
         this.introEthLogo.rotation.x += 0.36;
+        this.introEthLogo.position.z = 0.6;
         this.ethLogoLoaded = true;
         // console.log(gltf);
 
@@ -92,18 +123,9 @@ export default {
         //frameArea(boxSize * 0.5, boxSize, boxCenter, this.camera);
       });
 
-      // loader.load(coinurl, (gltf) => {
-      //   console.log(gltf);
-      // });
-
-      let geometry = new THREE.BoxGeometry(1, 1, 1);
-      let material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-      //this.cube = new THREE.Mesh(geometry, material);
-      //this.scene.add(this.cube);
-
       const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-      hemiLight.position.set(0, 20, 0);
-      //scene.add(hemiLight);
+      hemiLight.position.set(0, 0, 0);
+      this.scene.add(hemiLight);
 
       const dirLight = new THREE.DirectionalLight(0xffffff);
       dirLight.position.set(-3, 10, -10);
@@ -115,16 +137,17 @@ export default {
       dirLight.shadow.camera.near = 0.1;
       dirLight.shadow.camera.far = 40;
 
-      this.scene.add(dirLight);
+      //this.scene.add(dirLight);
       this.clock = new THREE.Clock();
       this.time = 0;
       this.delta = 0;
 
-      const size = 200;
-      const divisions = 200;
+      const size = 100;
+      const divisions = 90;
 
       const gridHelper = new THREE.GridHelper(size, divisions);
-      gridHelper.rotation.x += 0.2;
+      //gridHelper.rotation.x += 0.2;
+      gridHelper.position.y = 0;
       this.scene.add(gridHelper);
 
       this.animate();
@@ -141,7 +164,79 @@ export default {
       this.curScene = scene;
     },
 
+    addIntroBgObject(obj) {
+      // Add floating BG objects in intro scene
+      let randx = Math.random() * 10 + 3;
+      let randy = Math.random() * 5;
+      let randz = Math.random() * 15;
+      obj.position.x = Math.random() >= 0.5 ? 0 - randx : 0 + randx;
+      obj.position.y = Math.random() >= 0.5 ? 0 - randy : 0 + randy;
+      obj.position.z = -randz - 2;
+      obj.vertMoveMult = Math.random() * 0.01;
+      obj.vertDir = Math.random() >= 0.5 ? -1 : 1;
+      this.introBgObjects.push(obj);
+      this.scene.add(obj);
+
+      const box = new THREE.Box3().setFromObject(obj);
+      const boxSize = box.getSize(new THREE.Vector3()).length();
+      const boxCenter = box.getCenter(new THREE.Vector3());
+
+      var maxAxis = Math.max(boxSize.x, boxSize.y, boxSize.z);
+
+      obj.scale.multiplyScalar((1 / boxSize) * Math.random() * 7);
+      //obj.rotation.x += 0.36;
+    },
+
+    cloneGltf(gltf) {
+      const clone = {
+        animations: gltf.animations,
+        scene: gltf.scene.clone(true),
+      };
+
+      const skinnedMeshes = {};
+
+      gltf.scene.traverse((node) => {
+        if (node.isSkinnedMesh) {
+          skinnedMeshes[node.name] = node;
+        }
+      });
+
+      const cloneBones = {};
+      const cloneSkinnedMeshes = {};
+
+      clone.scene.traverse((node) => {
+        if (node.isBone) {
+          cloneBones[node.name] = node;
+        }
+
+        if (node.isSkinnedMesh) {
+          cloneSkinnedMeshes[node.name] = node;
+        }
+      });
+
+      for (let name in skinnedMeshes) {
+        const skinnedMesh = skinnedMeshes[name];
+        const skeleton = skinnedMesh.skeleton;
+        const cloneSkinnedMesh = cloneSkinnedMeshes[name];
+
+        const orderedCloneBones = [];
+
+        for (let i = 0; i < skeleton.bones.length; ++i) {
+          const cloneBone = cloneBones[skeleton.bones[i].name];
+          orderedCloneBones.push(cloneBone);
+        }
+
+        cloneSkinnedMesh.bind(
+          new Skeleton(orderedCloneBones, skeleton.boneInverses),
+          cloneSkinnedMesh.matrixWorld
+        );
+      }
+
+      return clone;
+    },
+
     updateIdle() {
+      //console.log(this.curScene);
       this.delta = this.clock.getDelta();
       this.time += this.delta;
 
@@ -153,14 +248,18 @@ export default {
       } else {
         mult = 15;
         this.introEthLogo.rotation.y += 0.01 * mult;
-        if (this.far < 60) {
-          this.far += 1;
+        if (this.far < 40) {
+          this.far += 0.6;
           //console.log(this.curScene);
+          this.scene.fog = new THREE.Fog(this.bgCol, 0, this.far);
         }
+        // Animate background objects
+        this.introBgObjects.forEach(function (obj) {
+          obj.position.y += obj.vertMoveMult * obj.vertDir;
+        });
       }
 
       this.introEthLogo.position.y = 0.2 * Math.abs(Math.sin(this.time) * 0.6);
-      this.scene.fog = new THREE.Fog(this.bgCol, 0, this.far);
     },
 
     updateIdleFadeout() {
@@ -168,8 +267,12 @@ export default {
       let mult = 15;
       this.introEthLogo.rotation.y += 0.01 * mult;
       this.introEthLogo.position.y = 0.2 * Math.abs(Math.sin(this.time) * 0.6);
-      if (this.far > 0) {
-        this.far -= 1;
+      // Animate background objects
+      this.introBgObjects.forEach(function (obj) {
+        obj.position.y += obj.vertMoveMult * obj.vertDir;
+      });
+      if (this.far > 0.6) {
+        this.far -= 0.6;
       } else {
         this.far = 0;
         setTimeout(() => {
